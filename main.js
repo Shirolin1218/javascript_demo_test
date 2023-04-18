4a37cffeaec83cb889731fd52e564772ab65efb4
Shirolin <jerrys40506@gmail.com>
2023-04-18 17:49:27 +0900

    test

--- main.js
+++ main.js
@@ -17,17 +17,19 @@ guessBtn.addEventListener("click", function () {
         max = guess;
         range.textContent = min + "-" + max;
         message.textContent = "太大";
-        guessInput.valueAsNumber=null;
-    }
-    if (guess > min && guess < answer) {
+        guessInput.valueAsNumber = null;
+    } else if (guess > min && guess < answer) {
         min = guess;
         range.textContent = min + "-" + max;
         message.textContent = "太小";
-        guessInput.valueAsNumber=null;
+        guessInput.valueAsNumber = null;
+    } else if (guess == answer) {
+        range.textContent = "恭喜答對"
+        message.textContent = "答案是" + answer;
+    }
+    else {
+        message.textContent = "請輸入" + min + "-" + max + "的數字";
+        guessInput.valueAsNumber = null;
     }
 
-    if (guess == answer) {
-        range.textContent = "恭喜答對"
-        message.textContent = "答案是"+answer;
-    }
 });
